312745a9a417f9a54f3510ace4c9102a5f4ca528
Helton Vasconcelos <heltonvasconcelos88@gmail.com>
2019-09-27 03:10:40 -0300

    adding patch route

--- backend/src/index.js
+++ backend/src/index.js
@@ -40,13 +40,37 @@ app.get('/users/:id', async (req, res) => {
   }
 });
 
+app.patch('/users/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'email', 'password', 'age'];
+  const isValidOperation = updates.every(update =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'wrong update keys!' });
+  }
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
 app.post('/tasks', async (req, res) => {
   const task = new Task(req.body);
   try {
     await task.save();
     res.status(201).send(task);
   } catch (e) {
-    res.status(400).send();
+    res.status(400).send(e);
   }
 });
 
@@ -72,6 +96,29 @@ app.get('/tasks/:id', async (req, res) => {
   }
 });
 
+app.patch('/tasks/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['completed', 'description'];
+  const isValidUpdate = updates.every(update =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidUpdate) {
+    return res.status(404).send({ error: 'wrong update keys!' });
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 app.listen(port, () => {
   console.log('Server is up on port ' + port);
 });
